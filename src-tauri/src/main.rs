22f21a7df53b1c8a11414ac89cecf6666af235a7
Nate Card <ncard24@gmail.com>
2023-11-30 22:53:10 -0500

    Add system tray functionality with custom menu items and icons

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,6 +1,7 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
-
+use tauri::SystemTray;
+use tauri::{CustomMenuItem, SystemTrayMenu, SystemTrayMenuItem}
 fn main() {
   tauri::Builder::default()
     .invoke_handler(tauri::generate_handler![counter])
@@ -12,3 +13,13 @@ fn main() {
 fn counter(value: i32) {
   println!("Cycle Counter: {value}")
 }
+
+let tray = SystemTray::new().with_menu(tray_menu)
+
+// here `"quit".to_string()` defines the menu item id, and the second parameter is the menu item label.
+let quit = CustomMenuItem::new("quit".to_string(), "Quit");
+let hide = CustomMenuItem::new("hide".to_string(), "Hide");
+let tray_menu = SystemTrayMenu::new()
+  .add_item(quit)
+  .add_native_item(SystemTrayMenuItem::Separator)
+  .add_item(hide);
