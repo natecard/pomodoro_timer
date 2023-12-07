3558129d73760a02c7b6fada9db012290d148c8b
Nate Card <ncard24@gmail.com>
2023-12-06 23:21:50 -0500

    Add key-value database functions

--- app/backend/database.ts
+++ app/backend/database.ts
@@ -16,3 +16,19 @@ const requestDB = openDB<SessionDB>("SessionLogs", 1, {
     db.createObjectStore("session");
   },
 });
+
+export async function get(key) {
+  return (await requestDB).get("keyval", key);
+}
+export async function set(key: string, val: number) {
+  return (await requestDB).put("keyval", val, key);
+}
+export async function del(key) {
+  return (await requestDB).delete("keyval", key);
+}
+export async function clear() {
+  return (await requestDB).clear("keyval");
+}
+export async function keys() {
+  return (await requestDB).getAllKeys("keyval");
+}
