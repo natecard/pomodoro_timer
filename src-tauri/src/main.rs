e96e8408ac89d4c52d204358f4cfbd887828dc7a
Nate Card <ncard24@gmail.com>
2023-12-07 15:23:07 -0500

    Refactor system tray menu and event handling code

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -4,7 +4,7 @@
 use std::env;
 // TODO Create window handler function
 
-use tauri::{Manager, AppHandle};
+use tauri::{Manager, AppHandle, Window};
 use tauri::{CustomMenuItem, SystemTrayMenu, SystemTrayMenuItem, SystemTrayEvent, SystemTray};
 fn main() {
  let icon_bytes = if cfg!(windows) {
@@ -16,41 +16,26 @@ fn main() {
   let icon = tauri::Icon::Raw(icon_bytes);
 
 
-  let tray = SystemTray::new()
-   .with_icon(icon);
-
+  
+  
   let quit = CustomMenuItem::new("quit".to_string(), "Quit");
   let hide = CustomMenuItem::new("hide".to_string(), "Hide");
-  let tray_menu = SystemTrayMenu::new()// insert the menu items here
-  .add_item(quit)
+  let open_window: CustomMenuItem = CustomMenuItem::new("open".to_string(), "Open");
+  
+  let tray_menu = SystemTrayMenu::new()
+  .add_item(open_window)
+  .add_item(hide)
   .add_native_item(SystemTrayMenuItem::Separator)
-  .add_item(hide);
+  .add_item(quit);
 
+  let system_tray = SystemTray::new().with_menu(tray_menu)
+  .with_icon(icon);
+  
   tauri::Builder::default()
-    .system_tray(SystemTray::new().with_menu(tray_menu))
-    .on_system_tray_event(|app, event| match event {
-      SystemTrayEvent::LeftClick {
-        position: _,
-        size: _,
-        ..
-      } => {
-        println!("system tray received a left click");
-      }
-      SystemTrayEvent::RightClick {
-        position: _,
-        size: _,
-        ..
-      } => {
-        println!("system tray received a right click");
-      }
-      SystemTrayEvent::DoubleClick {
-        position: _,
-        size: _,
-        ..
-      } => {
-        println!("system tray received a double click");
-      }
-      SystemTrayEvent::MenuItemClick { id, .. } => {
+  .system_tray(system_tray.clone())
+  .on_system_tray_event(|app, event| match event {
+    SystemTrayEvent::MenuItemClick { id, .. } => {
+        // let item_handle = app.tray_handle().get_item(&id);
         match id.as_str() {
           "quit" => {
             std::process::exit(0);
@@ -59,13 +44,17 @@ fn main() {
             let window = app.get_window("main").unwrap();
             window.hide().unwrap();
           }
+          "open" => {
+            let window: Window = app.get_window("main").unwrap();
+            window.show().unwrap();
+          }
           _ => {}
         }
       }
       _ => {}
     })
     .invoke_handler(tauri::generate_handler![break_counter, session_counter, show_window])
-    .system_tray(tray)
+    .system_tray(system_tray.clone())
     .run(tauri::generate_context!())
     .expect("error while running tauri application");
 }
